856d7d7a180efe2e6095e53dd76f228348f52cb2
Grzegorz Wojciechowski <gwojciech@newterm.pl>
2017-08-16 23:16:07 +0200

    QtGame: game scene + displaying hexagon
    *Added displaying game scene
    *Added displaying simple hexagon

---
+++ QtGame/game.cpp
@@ -0,0 +1,23 @@
+#include <game.h>
+#include <hex.h>
+
+Game::Game(QWidget *parent) {
+    // set up the screen
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setFixedSize(1024,768);
+
+    // set up the scene
+    scene = new QGraphicsScene();
+    scene->setSceneRect(0,0,1024,768);
+    setScene(scene);
+
+}
+
+void Game::start() {
+    // test code TODO remove
+    Hex* hex = new Hex();
+    scene->addItem(hex);
+    hex->setPos(100,100);
+
+}
